6c64f64255fd12575c3e6e77eb12b982b9d37607
geeks0n-byte <giga.sichinava@gmail.com>
2026-09-25 01:18:50 +0200

    Publish the latest Spaceblox web export.
    Built from spaceblox main bf97724872fb8123369ae6bb3101eb03fdc8ea7d with the non-mono editor. The play shell still letterboxes wide windows.

--- spaceblox/play/index.service.worker.js
+++ spaceblox/play/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1790246363|66803721';
+const CACHE_VERSION = '1790291887|17389499';
 /** @type {string} */
 const CACHE_PREFIX = 'Spaceblox-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
@@ -14,7 +14,7 @@ const OFFLINE_URL = 'index.offline.html';
 const ENSURE_CROSSORIGIN_ISOLATION_HEADERS = false;
 // Files that will be cached on load.
 /** @type {string[]} */
-const CACHED_FILES = ["index.html","index.js","index.offline.html","index.icon.png","index.apple-touch-icon.png","index.audio.worklet.js","index.audio.position.worklet.js","PressStart2P.ttf","spaceblox_ads.js","og-share.png"];
+const CACHED_FILES = ["index.html","index.js","index.offline.html","index.icon.png","index.apple-touch-icon.png","index.audio.worklet.js","index.audio.position.worklet.js"];
 // Files that we might not want the user to preload, and will only be cached on first load.
 /** @type {string[]} */
 const CACHEABLE_FILES = ["index.wasm","index.pck"];
